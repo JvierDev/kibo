f8e43688763e7e10f37a0a5371b315be6d5ce07a
Javier Dev <ljavier@hotmail.cl>
2026-09-04 17:16:52 -0400

    feat: initialize Electron app with React and TypeScript

---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,12 @@
+import Versions from './components/Versions'
+
+function App(): React.JSX.Element {
+  return (
+    <div className="app">
+      <h1>Kibo</h1>
+      <Versions />
+    </div>
+  )
+}
+
+export default App
